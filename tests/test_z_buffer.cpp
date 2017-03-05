7114104fc8c2e6a5f096f3584adf10ae96b5e70e
Scott Munro <scottnmunro@gmail.com>
2017-03-04 18:52:08 -0600

    added a random access test for the zbuffer

--- tests/test_z_buffer.cpp
+++ tests/test_z_buffer.cpp
@@ -1,7 +1,7 @@
 #include "../z_buffer.hpp"
 #include "gtest/gtest.h"
 
-TEST(ZBufferTests, ZBufferTestInit)
+TEST(ZBufferTests, ZBuffer_Test_Init)
 {
     auto w = 10;
     auto h = 10;
@@ -15,4 +15,16 @@ TEST(ZBufferTests, ZBufferTestInit)
                     get_z_buffer(test_buffer, x, y));
         }
     }
+}
+
+TEST(ZBufferTests, ZBuffer_Test_Random_Elem)
+{
+    auto w = 10;
+    auto h = 10;
+    z_buffer test_buffer(w, h);
+    
+    set_z_buffer(test_buffer, 2, 2, 1.5f);
+    EXPECT_EQ(get_z_buffer(test_buffer, 2, 2), 1.5f);
+    EXPECT_EQ(get_z_buffer(test_buffer, 3, 2),
+            std::numeric_limits<float>::lowest());
 }
