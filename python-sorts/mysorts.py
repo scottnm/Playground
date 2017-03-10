a4a10e7fd4e39a456d1a928ac9a90422993b5366
Scott Munro <scottnmunro@gmail.com>
2017-03-10 10:46:38 -0600

    added a bubble sort implementation

--- python-sorts/mysorts.py
+++ python-sorts/mysorts.py
@@ -1,1 +1,19 @@
 #!/usr/bin/env python3
+
+import operator
+from typing import Any, List, Callable
+
+def bubble(__seq : List[Any], *, \
+           out_of_order : Callable = operator.gt) -> List[Any]:
+    seq = __seq[:]
+    for unsorted_range in range(len(__seq), 0, -1):
+        already_sorted = True
+        for i in range(0, unsorted_range - 1):
+            if out_of_order(seq[i], seq[i + 1]):
+                already_sorted = False
+                seq[i], seq[i + 1] = seq[i + 1], seq[i]
+        if already_sorted:
+            break
+    return seq
+
+    
--- python-sorts/test_sorts.py
+++ python-sorts/test_sorts.py
@@ -2,12 +2,12 @@
 
 
 from mysorts import *
+from typing import Callable, List
 import unittest
 
 class TestSorts(unittest.TestCase):
     # sorts go here
-    sorts = [ \
-            ]
+    sorts = [bubble]
 
     def test_simple_presorted(self):
         input = [i for i in range(1, 5)]
@@ -16,8 +16,8 @@ class TestSorts(unittest.TestCase):
         assert input is not expected_output
         for sort_func in TestSorts.sorts:
             with self.subTest():
-                self.assertEquals(expected_output, sort_func(input))
-                self.assertEquals(input, preserved_input) # assert no mutation
+                self.assertEqual(expected_output, sort_func(input))
+                self.assertEqual(input, preserved_input) # assert no mutation
 
     def test_simple_reversed(self):
         input = [i for i in range(4, 0, -1)]
@@ -25,8 +25,8 @@ class TestSorts(unittest.TestCase):
         expected_output = [i for i in range(1, 5)]
         for sort_func in TestSorts.sorts:
             with self.subTest():
-                self.assertEquals(expected_output, sort_func(input))
-                self.assertEquals(input, preserved_input) # assert no mutation
+                self.assertEqual(expected_output, sort_func(input))
+                self.assertEqual(input, preserved_input) # assert no mutation
 
     def test_simple_random_arrangement(self):
         input = [0, 4, 7, 3, 2, 8, 5, 1, 9, 6]
@@ -34,8 +34,8 @@ class TestSorts(unittest.TestCase):
         expected_output = [i for i in range(0, 10)]
         for sort_func in TestSorts.sorts:
             with self.subTest():
-                self.assertEquals(expected_output, sort_func(input))
-                self.assertEquals(input, preserved_input) # assert no mutation
+                self.assertEqual(expected_output, sort_func(input))
+                self.assertEqual(input, preserved_input) # assert no mutation
 
 
 if __name__ == "__main__":
