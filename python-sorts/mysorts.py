1a8dba6cedf09d5db4edd789c6563868153d692e
Scott Munro <scottnmunro@gmail.com>
2017-03-12 12:30:31 -0500

    added tests for variadic order operator

--- python-sorts/mysorts.py
+++ python-sorts/mysorts.py
@@ -6,12 +6,12 @@ from typing import Any, Callable, List, TypeVar
 T = TypeVar('T')
 
 def bubble(__seq : List[T], *, \
-           comp_gt : Callable[[T, T], bool] = operator.gt) -> List[T]:
+           order_gt : Callable[[T, T], bool] = operator.gt) -> List[T]:
     seq = __seq[:]
     for unsorted_range in range(len(__seq), 0, -1):
         already_sorted = True
         for i in range(0, unsorted_range - 1):
-            if comp_gt(seq[i], seq[i + 1]):
+            if order_gt(seq[i], seq[i + 1]):
                 already_sorted = False
                 seq[i], seq[i + 1] = seq[i + 1], seq[i]
         if already_sorted:
@@ -19,24 +19,24 @@ def bubble(__seq : List[T], *, \
     return seq
 
 def selection(__seq : List[T], *, \
-              comp_gt : Callable[[T, T], bool] = operator.gt) -> List[T]:
+              order_gt : Callable[[T, T], bool] = operator.gt) -> List[T]:
     seq = __seq[:]
     for unsorted_mark in range(0, len(__seq) - 1):
         min_index = unsorted_mark
         for i in range(unsorted_mark + 1, len(__seq)):
-            if comp_gt(seq[min_index], seq[i]):
+            if order_gt(seq[min_index], seq[i]):
                 min_index = i
         seq[unsorted_mark], seq[min_index] = seq[min_index], seq[unsorted_mark]
     return seq
 
 def insertion(__seq : List[T], *, \
-              comp_gt : Callable[[T, T], bool] = operator.gt) -> List[T]:
+              order_gt : Callable[[T, T], bool] = operator.gt) -> List[T]:
     seq = __seq[:]
     for unsorted_mark in range(1, len(__seq)):
         ele = seq[unsorted_mark]
         insert_pos = 0
         for i in range(unsorted_mark - 1, -1, -1):
-            if comp_gt(seq[i], ele):
+            if order_gt(seq[i], ele):
                 seq[i + 1] = seq[i]
             else:
                 insert_pos = i + 1
@@ -45,7 +45,7 @@ def insertion(__seq : List[T], *, \
     return seq
 
 def quick_1(__seq : List[T], *, \
-          comp_gt : Callable[[T, T], bool] = operator.gt) -> List[T]:
+          order_gt : Callable[[T, T], bool] = operator.gt) -> List[T]:
 
     seq = __seq[:]
     sublist_stack = [(0, len(__seq))] 
@@ -65,12 +65,12 @@ def quick_1(__seq : List[T], *, \
         ele_gt = 0 # keep track of elements greater than the pivot
 
         for i in range(sublist[0], pivot):
-            if comp_gt(seq[i], pivot_ele):
+            if order_gt(seq[i], pivot_ele):
                 ele_gt += 1
             else:
                 ele_le += 1
         for i in range(pivot+1, sublist[1]):
-            if comp_gt(seq[i], pivot_ele):
+            if order_gt(seq[i], pivot_ele):
                 ele_gt += 1
             else:
                 ele_le += 1
@@ -88,9 +88,9 @@ def quick_1(__seq : List[T], *, \
             le_runner = 0
             gt_runner = 0
             while gt_swaps > 0 and le_runner < ele_le:
-                if comp_gt(seq[left_0 + le_runner], pivot_ele):
+                if order_gt(seq[left_0 + le_runner], pivot_ele):
                     while True:
-                        if not comp_gt(seq[right_0 + gt_runner], pivot_ele):
+                        if not order_gt(seq[right_0 + gt_runner], pivot_ele):
                             break
                         else:
                             gt_runner += 1
@@ -105,9 +105,9 @@ def quick_1(__seq : List[T], *, \
             le_runner = 0
             gt_runner = 0
             while le_swaps > 0 and gt_runner < ele_gt:
-                if not comp_gt(seq[right_0 + gt_runner], pivot_ele):
+                if not order_gt(seq[right_0 + gt_runner], pivot_ele):
                     while True:
-                        if comp_gt(seq[left_0 + le_runner], pivot_ele):
+                        if order_gt(seq[left_0 + le_runner], pivot_ele):
                             break
                         else:
                             le_runner += 1
@@ -123,7 +123,7 @@ def quick_1(__seq : List[T], *, \
     return seq
 
 def quick_2(__seq : List[T], *, \
-          comp_gt : Callable[[T, T], bool] = operator.gt) -> List[T]:
+          order_gt : Callable[[T, T], bool] = operator.gt) -> List[T]:
     if len(__seq) < 2:
         return __seq
 
@@ -134,16 +134,16 @@ def quick_2(__seq : List[T], *, \
 
     for i in range(0, pivot):
         ele = __seq[i]
-        if comp_gt(ele, pivot_ele):
+        if order_gt(ele, pivot_ele):
             gt_sublist.append(ele)
         else:
             le_sublist.append(ele)
     for i in range(pivot+1, len(__seq)):
         ele = __seq[i]
-        if comp_gt(ele, pivot_ele):
+        if order_gt(ele, pivot_ele):
             gt_sublist.append(ele)
         else:
             le_sublist.append(ele)
 
-    return quick_2(le_sublist, comp_gt=comp_gt) + [pivot_ele] + \
-            quick_2(gt_sublist, comp_gt=comp_gt)
+    return quick_2(le_sublist, order_gt=order_gt) + [pivot_ele] + \
+            quick_2(gt_sublist, order_gt=order_gt)
--- python-sorts/test_sorts.py
+++ python-sorts/test_sorts.py
@@ -4,17 +4,18 @@
 from mysorts import *
 from typing import Callable, List
 import unittest
+import operator
 
 class TestSorts(unittest.TestCase):
     # sorts go here
     sorts = [bubble, selection, insertion, quick_1, quick_2]
 
-    def sort_test(self, input, expected_output):
+    def sort_test(self, input, expected_output, order_gt=operator.gt):
         preserved_input = input[:]
         assert input is not expected_output
         for sort_func in TestSorts.sorts:
             with self.subTest(msg=sort_func.__name__):
-                self.assertEqual(expected_output, sort_func(input))
+                self.assertEqual(expected_output, sort_func(input, order_gt=order_gt))
                 self.assertEqual(input, preserved_input) # assert no mutation
 
     def test_1_element(self):
@@ -36,6 +37,18 @@ class TestSorts(unittest.TestCase):
         input = [0, 4, 7, 3, 2, 8, 5, 1, 9, 6]
         expected_output = [i for i in range(0, 10)]
         self.sort_test(input, expected_output)
+
+    def test_reverse_sort_presorted(self):
+        input = [i for i in range(4, 0, -1)]
+        expected_output = input[:]
+        self.sort_test(input, expected_output, operator.le)
+        pass
+
+    def test_reverse_sort_reversed(self):
+        input = [i for i in range(1, 5)]
+        expected_output = [i for i in range(4, 0, -1)]
+        self.sort_test(input, expected_output, operator.le)
+        pass
 
 
 if __name__ == "__main__":
