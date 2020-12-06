2f356baecf8dfc26b2ccee4b319aabd69b2cba12
Scott Munro <scottnmunro@gmail.com>
2020-12-05 23:39:02 -0800

    called loadlibrary using c ffi to make dlls available in my rust exe

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use std::mem::transmute;
+
+type HModule = *const std::ffi::c_void;
+type FarProc = *const std::ffi::c_void;
+type Fn_MessageBoxA = extern "stdcall" fn(*const std::ffi::c_void, *const u8, *const u8, u32);
+
+extern "stdcall" {
+    fn LoadLibraryA(name: *const u8) -> HModule;
+    fn GetProcAddress(module: HModule, name: *const u8) -> FarProc;
+}
+
 fn main() {
-    println!("Hello, world!");
+    let h: HModule = unsafe { LoadLibraryA("IPHLPAPI.dll\0".as_ptr()) };
+    let user32h: HModule = unsafe { LoadLibraryA("USER32.dll\0".as_ptr()) };
+    let MessageBoxFnPtr: FarProc = unsafe { GetProcAddress(user32h, "MessageBoxA\0".as_ptr()) };
+    let MessageBoxA: Fn_MessageBoxA = unsafe { transmute(MessageBoxFnPtr) };
+
+    MessageBoxA(
+        std::ptr::null(),
+        "Hello from Rust!\0".as_ptr(),
+        std::ptr::null(),
+        0,
+    );
+
+    println!(
+        "h = {:?}, user32h = {:?}, MessageBoxA = {:?}",
+        h, user32h, MessageBoxFnPtr
+    );
 }
