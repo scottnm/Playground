883d3df6d5286ef1e83bc1c9cabb9094b4f76580
Scott Munro <scottnmunro@gmail.com>
2020-12-12 21:29:58 -0800

    use the wmi crate instead of raw wmi queries

--- ersatz/src/main.rs
+++ ersatz/src/main.rs
@@ -3,60 +3,44 @@ extern crate rawsock;
 extern crate serde;
 extern crate wmi;
 
-fn wmic(args: &[&str]) -> anyhow::Result<String> {
-    let cmd_output = std::process::Command::new("wmic").args(args).output()?;
+fn main() -> anyhow::Result<()> {
+    let wmi_com = wmi::COMLibrary::new()?;
+    let wmi_connection = wmi::WMIConnection::new(wmi_com.into())?;
 
-    let stdout = std::str::from_utf8(&cmd_output.stdout)?;
-    let stderr = std::str::from_utf8(&cmd_output.stderr)?;
-
-    match cmd_output.status.code() {
-        Some(0) => (), // all good
-        Some(n) => panic!("wmic failure ({})!\n{}", n, stderr),
-        _ => panic!("wmic failure!\n{}", stderr),
+    #[derive(serde::Deserialize, Debug)]
+    #[allow(non_camel_case_types, non_snake_case)]
+    struct Win32_IP4RouteTable {
+        InterfaceIndex: i64,
     }
 
-    let response = stdout
-        .split("\n")
-        .map(|s| s.trim())
-        .filter(|s| !s.is_empty())
-        .last()
-        .unwrap();
+    let default_nic_index_query: Vec<Win32_IP4RouteTable> = wmi_connection
+        .raw_query("SELECT * FROM Win32_IP4RouteTable Where Destination='0.0.0.0'")?;
+    let default_nic_index = default_nic_index_query[0].InterfaceIndex;
+    println!("Default nic index: {}", default_nic_index);
 
-    Ok(response.to_owned())
-}
+    #[derive(serde::Deserialize, Debug)]
+    #[allow(non_camel_case_types, non_snake_case)]
+    struct Win32_NetworkAdapter {
+        guid: String,
+    }
 
-fn main() -> anyhow::Result<()> {
-    let interface_index_str = wmic(&[
-        "Path",
-        "Win32_IP4RouteTable",
-        "Where",
-        "Destination='0.0.0.0'",
-        "Get",
-        "InterfaceIndex",
-    ])?;
+    let default_nic_query_str = std::format!(
+        "SELECT * FROM Win32_NetworkAdapter Where InterfaceIndex={}",
+        default_nic_index,
+    );
+    let default_nic_query: Vec<Win32_NetworkAdapter> =
+        wmi_connection.raw_query(&default_nic_query_str)?;
+    let default_nic_guid_str = &default_nic_query[0].guid;
+    println!("Default nic guid: {}", default_nic_guid_str);
 
-    println!("Default interface index: {}", interface_index_str);
-
-    let interface_query = std::format!("InterfaceIndex={}", interface_index_str);
-    let nic_guid_str = wmic(&[
-        "Path",
-        "Win32_NetworkAdapter",
-        "Where",
-        &interface_query,
-        "Get",
-        "Guid",
-    ])?;
-
-    println!("NIC guid: {}", nic_guid_str);
-
-    let nic_name = std::format!(r#"\Device\NPF_{}"#, nic_guid_str);
-    println!("NIC name: {}", nic_name);
+    let default_nic_name = std::format!(r#"\Device\NPF_{}"#, default_nic_guid_str);
+    println!("NIC name: {}", default_nic_name);
 
     let lib = rawsock::open_best_library()?;
 
     let nics = lib.all_interfaces()?;
-    let nic = nics.iter().find(|n| n.name == nic_name).unwrap();
+    let nic = nics.iter().find(|n| n.name == default_nic_name).unwrap();
 
-    println!("Default interface: {:?}", nic);
+    println!("Default nic: {:?}", nic);
     Ok(())
 }
