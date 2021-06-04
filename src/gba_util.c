4dff522caa2215c3f2299f26dfcccc2a630c4594
Scott Munro <scottnmunro@gmail.com>
2021-06-04 11:42:55 -0700

    added assert about calling mode3 screen buffer outside of the right mode

--- src/gba_util.c
+++ src/gba_util.c
@@ -9,6 +9,27 @@ typedef uint32_t register_t;
 
 static register_t* s_display_control_register = (register_t*)0x4000000; // REG_DISPCNT
 
+typedef struct display_mode_t {
+    video_mode_t video_mode;
+    bg_mode_t bg_mode;
+} display_mode_t;
+
+display_mode_t
+get_gba_display_mode()
+{
+    const register_t display_control = *s_display_control_register;
+
+    // First 3 bits are the video mode
+    const video_mode_options_t video_mode = display_control & 0x7;
+    // Bits 0x8->0xA are the bg_mode
+    const bg_mode_options_t bg_mode = (display_control >> 0x8) & 0xF;
+
+    return (display_mode_t) {
+        .video_mode = video_mode,
+        .bg_mode = bg_mode,
+    };
+}
+
 void
 set_gba_display_mode(
     video_mode_t video_mode,
@@ -30,6 +51,7 @@ set_gba_display_mode(
 u16_span_t
 get_gba_mode3_screen_buffer()
 {
+    dbg_assert(get_gba_display_mode().video_mode.value == VIDEO_MODE_MODE3_16BIT_BMP);
     // GBA's VRAM starts at 0x6000000
     return (u16_span_t) {
         .data = (uint16_t*)0x6000000,
