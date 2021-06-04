5a5207ab04a9be529e1c4ad28016510a39887b24
Scott Munro <scottnmunro@gmail.com>
2021-06-04 08:14:41 -0700

    rearrange helpful constants

--- src/gba_util.h
+++ src/gba_util.h
@@ -1,6 +1,11 @@
 #ifndef __GBA_UTIL_H__
 #define __GBA_UTIL_H__
 
+// N.B. can't be `static const` values since C requires such values to known at compile time and C doesn't
+// have a way to define compile-time, computed constants
+#define COLOR_BLUE rgb16(0, 0, 31)
+#define COLOR_WHITE rgb16(31, 31, 31)
+
 static
 inline
 uint16_t
@@ -22,9 +27,6 @@ uint32_t
 get_gba_pixel_index(
     uint8_t row,
     uint8_t col);
-
-#define COLOR_BLUE rgb16(0, 0, 31)
-#define COLOR_WHITE rgb16(31, 31, 31)
 
 #endif // __GBA_UTIL_H__
 
