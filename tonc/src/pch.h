c5a064efa0f8df1927002c7877f995daf4561677
scott munro <scottnmunro@gmail.com>
2021-06-06 10:22:19 -0700

    style cleanup

--- tonc/src/pch.h
+++ tonc/src/pch.h
@@ -1,9 +1,9 @@
 #pragma once
 
-#include <gba_console.h>
-#include <gba_video.h>
-#include <gba_interrupt.h>
-#include <gba_systemcalls.h>
-#include <gba_input.h>
+// c-std includes
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
+
+// project includes
+#include "snm_gba_util.h"
--- tonc/src/template.c
+++ tonc/src/template.c
@@ -1,27 +1,24 @@
 #include "pch.h"
 
-//---------------------------------------------------------------------------------
-// Program entry point
-//---------------------------------------------------------------------------------
-int main(void) {
-//---------------------------------------------------------------------------------
+int
+main(
+    void)
+{
+    // The vblank interrupt must be enabled before calling snmgba_wait_for_vblank_interrupt(..). Though we don't need to
+    // set a handler for this interrupt since the default dispatcher handles the bios flags.
+    snmgba_init_interrupt_system();
+    snmgba_enable_interrupt(SNMGBA_INTERRUPT_MASK_ENABLE_VBLANK_INTERRUPTS);
 
+    snmgba_setup_simple_screen(); // A basic blue screen
 
-	// the vblank interrupt must be enabled for VBlankIntrWait() to work
-	// since the default dispatcher handles the bios flags no vblank handler
-	// is required
-	irqInit();
-	irqEnable(IRQ_VBLANK);
+    // ansi escape sequence to set print co-ordinates
+    // /x1b[line;columnH
+    iprintf("\x1b[10;10HHello World!\n");
 
-	consoleDemoInit();
+    while (true)
+    {
+        snmgba_wait_for_vblank_interrupt();
+    }
 
-	// ansi escape sequence to set print co-ordinates
-	// /x1b[line;columnH
-	iprintf("\x1b[10;10HHello World!\n");
-
-	while (1) {
-		VBlankIntrWait();
-	}
+    return 0;
 }
-
-
