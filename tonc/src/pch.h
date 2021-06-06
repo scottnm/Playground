10c0cf0bef40020795a9ad6e1417ee5949232286
scott munro <scottnmunro@gmail.com>
2021-06-06 09:29:49 -0700

    moved everything to a pch file

---
+++ tonc/src/pch.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <gba_console.h>
+#include <gba_video.h>
+#include <gba_interrupt.h>
+#include <gba_systemcalls.h>
+#include <gba_input.h>
+#include <stdio.h>
+#include <stdlib.h>
--- tonc/src/template.c
+++ tonc/src/template.c
@@ -1,11 +1,4 @@
-
-#include <gba_console.h>
-#include <gba_video.h>
-#include <gba_interrupt.h>
-#include <gba_systemcalls.h>
-#include <gba_input.h>
-#include <stdio.h>
-#include <stdlib.h>
+#include "pch.h"
 
 //---------------------------------------------------------------------------------
 // Program entry point
