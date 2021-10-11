1b33e6c23ff58b98f8d13515050aa2955573dfa8
scott munro <scottnmunro@gmail.com>
2021-10-10 22:12:28 -0700

    debug print the incoming bytes

--- cs_server/Program.cs
+++ cs_server/Program.cs
@@ -25,8 +25,30 @@ namespace cs_server
             serverTcpSocket.Start();
             Console.WriteLine("Server has started on {0}! Waiting for connection...", GetAddressString(serverTcpSocket));
 
-            System.Net.Sockets.TcpClient acceptedClient = serverTcpSocket.AcceptTcpClient();
+            System.Net.Sockets.TcpClient client = serverTcpSocket.AcceptTcpClient();
             Console.WriteLine("A client connected!");
+
+            System.Net.Sockets.NetworkStream clientStream = client.GetStream();
+
+            while (true)
+            {
+                int blockCount = 0;
+                Console.Write("Waiting for stream data...");
+                while (!clientStream.DataAvailable)
+                {
+                    System.Threading.Thread.Sleep(100); // poll for new data every 100ms
+                    blockCount += 1;
+                    if (blockCount % 10 == 0)
+                    {
+                        Console.Write(".");
+                    }
+                }
+                Console.WriteLine("");
+
+                Byte[] readBytes = new Byte[client.Available];
+                clientStream.Read(readBytes, 0, readBytes.Length);
+                Console.WriteLine("Read bytes: {0}", System.Text.Encoding.UTF8.GetString(readBytes, 0, readBytes.Length));
+            }
         }
     }
 }
