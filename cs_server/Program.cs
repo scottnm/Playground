f822546822c774f786f7844acef58d72ea54d8de
scott munro <scottnmunro@gmail.com>
2021-10-10 17:53:50 -0700

    made a simple c# tcp listener (the beginnings of a websocket server)

---
+++ cs_server/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace cs_server
+{
+    internal class Program
+    {
+        static
+        string
+        GetAddressString(
+            System.Net.Sockets.TcpListener tcpListener)
+        {
+            var ipEndpoint = tcpListener.LocalEndpoint as System.Net.IPEndPoint;
+            return String.Format("{0}:{1}", ipEndpoint.Address, ipEndpoint.Port);
+        }
+
+        static
+        void
+        Main(
+            string[] args)
+        {
+            var serverTcpSocket = new System.Net.Sockets.TcpListener(System.Net.IPAddress.Parse("127.0.0.1"), 80);
+
+            serverTcpSocket.Start();
+            Console.WriteLine("Server has started on {0}! Waiting for connection...", GetAddressString(serverTcpSocket));
+
+            System.Net.Sockets.TcpClient acceptedClient = serverTcpSocket.AcceptTcpClient();
+            Console.WriteLine("A client connected!");
+        }
+    }
+}
