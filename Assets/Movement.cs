c29f68baef28a505f7192468bf583e4f6165c87a
scott munro <scottnmunro@gmail.com>
2022-05-14 16:05:58 -0700

    fix rotations to apply relative to the player and not the world

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -14,9 +14,11 @@ public class Movement : MonoBehaviour
     {
         const float PITCH_MAX = 0.1f;
         const float ROLL_MAX = 0.1f;
+        const float FORWARD_SPEED = 2.0f;
+
         var pitch = PITCH_MAX * Input.GetAxis("Vertical");
-        var roll = ROLL_MAX * Input.GetAxis("Horizontal");
-        transform.Rotate(-pitch, 0, -roll); // invert the pitch and roll for more natural rotation
-        transform.position += transform.forward * Time.deltaTime;
+        var roll = ROLL_MAX * Input.GetAxis("Horizontal") * -1; // -1 to invert roll for more natural feel
+        transform.Rotate(pitch, 0, roll, Space.Self);
+        transform.position += transform.forward * Time.deltaTime * FORWARD_SPEED;
     }
 }
