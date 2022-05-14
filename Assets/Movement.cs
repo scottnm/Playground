a1c20363f79b27d8966af25e58d4c20c3687268e
scott munro <scottnmunro@gmail.com>
2022-05-14 16:03:25 -0700

    now we can pitch and roll in 3d space to move around

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -12,6 +12,11 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position += Vector3.forward * Time.deltaTime;
+        const float PITCH_MAX = 0.1f;
+        const float ROLL_MAX = 0.1f;
+        var pitch = PITCH_MAX * Input.GetAxis("Vertical");
+        var roll = ROLL_MAX * Input.GetAxis("Horizontal");
+        transform.Rotate(-pitch, 0, -roll); // invert the pitch and roll for more natural rotation
+        transform.position += transform.forward * Time.deltaTime;
     }
 }
