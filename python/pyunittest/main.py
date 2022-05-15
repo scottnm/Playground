0abd36a470c814f81ee3ab03d67f2f4ca5cc0cd0
scott munro <scottnmunro@gmail.com>
2022-05-15 13:28:18 -0700

    add pyunittest demo/playground

---
+++ python/pyunittest/main.py
@@ -0,0 +1,7 @@
+import mymod
+
+def main():
+    print("hello world {} {}".format(mymod.some_func(), mymod.some_other_func()))
+
+if __name__ == "__main__":
+    main()
---
+++ python/pyunittest/mymod.py
@@ -0,0 +1,5 @@
+def some_func():
+    return 1 + 2
+
+def some_other_func():
+    return 2 + 5
