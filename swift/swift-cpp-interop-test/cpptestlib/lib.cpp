afe4ed06f9e1c0db89ed4722924faf6d961c451c
Scott Munro <scottnmunro@gmail.com>
2025-11-16 15:37:30 -0800

    stub library

--- swift/swift-cpp-interop-test/cpptestlib/lib.cpp
+++ swift/swift-cpp-interop-test/cpptestlib/lib.cpp
@@ -1,1 +1,40 @@
+#include "lib.h"
+#include <cstdio>
 
+namespace CppTestLib
+{
+
+struct SomeStruct
+{
+    int ival;
+    char cval;
+};
+
+void TakeSomeCppParams(const std::string& str)
+{
+    printf("=> %30s :: %s\n", __PRETTY_FUNCTION__, str.c_str());
+}
+
+void TakeAPtrParam(const int* ip)
+{
+    printf("=> %30s :: %i\n", __PRETTY_FUNCTION__, *ip);
+}
+
+void TakeAStructPtrParam(const SomeStruct* sp)
+{
+    printf("=> %30s :: {i=%i, c=%c}\n", __PRETTY_FUNCTION__, sp->ival, sp->cval);
+}
+
+void OutStructPtrParam(SomeStruct* osp)
+{
+    osp->cval = 'o';
+    osp->ival = 10;
+    printf("<= %30s :: {i=%i, c=%c}\n", __PRETTY_FUNCTION__, osp->ival, osp->cval);
+}
+
+int ReturnSomePodValue()
+{
+    constexpr int c_someVal = 20;
+    printf("<= %30s :: %i\n", __PRETTY_FUNCTION__, c_someVal);
+    return c_someVal;
+}
--- swift/swift-cpp-interop-test/cpptestlib/lib.h
+++ swift/swift-cpp-interop-test/cpptestlib/lib.h
@@ -1,1 +1,16 @@
+#include <string>
 
+namespace CppTestLib
+{
+    struct SomeStruct
+    {
+        int ival;
+        char cval;
+    };
+
+    void TakeSomeCppParams(const std::string& str);
+    void TakeAPtrParam(const int* ip);
+    void TakeAStructPtrParam(const SomeStruct* sp);
+    void OutStructPtrParam(SomeStruct* osp);
+    int ReturnSomePodValue();
+}
