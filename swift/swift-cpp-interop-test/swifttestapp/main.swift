07ca7e3a520ec982af2fac0df9d969a08064118a
Scott Munro <scottnmunro@gmail.com>
2025-11-16 20:05:35 -0800

    a first c++ interop call

--- swift/swift-cpp-interop-test/swifttestapp/main.swift
+++ swift/swift-cpp-interop-test/swifttestapp/main.swift
@@ -1,3 +1,17 @@
 import cpptestlib
+import CxxStdlib
 
+cpptestlib.CppTestLib.TakeSomeCppParams("test123");
+// 
+// int i = 5;
+// CppTestLib::TakeAPtrParam(&i);
+// 
+// CppTestLib::SomeStruct inVal{30, 'Z'};
+// CppTestLib::TakeAStructPtrParam(&inVal);
+// 
+// CppTestLib::SomeStruct outVal;
+// CppTestLib::OutStructPtrParam(&outVal);
+// 
+// const int rv = CppTestLib::ReturnSomePodValue(10);
+// NSLog(@".. received %i from CppTestLib::ReturnSomePodValue", rv);
 print("Hello, World!"); 
