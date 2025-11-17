fa835f73ef7b520d0a7b6544ef03d82af196e64e
Scott Munro <scottnmunro@gmail.com>
2025-11-16 19:31:15 -0800

    calling c++ from objective-c++

--- swift/swift-cpp-interop-test/cpptestlib/lib.cpp
+++ swift/swift-cpp-interop-test/cpptestlib/lib.cpp
@@ -6,7 +6,7 @@ namespace CppTestLib
 
 void TakeSomeCppParams(const std::string& str)
 {
-    printf("=> %30s :: %s\n", __PRETTY_FUNCTION__, str.c_str());
+    printf("=> %30s :: \"%s\"\n", __PRETTY_FUNCTION__, str.c_str());
 }
 
 void TakeAPtrParam(const int* ip)
@@ -16,14 +16,14 @@ void TakeAPtrParam(const int* ip)
 
 void TakeAStructPtrParam(const SomeStruct* sp)
 {
-    printf("=> %30s :: {i=%i, c=%c}\n", __PRETTY_FUNCTION__, sp->ival, sp->cval);
+    printf("=> %30s :: {%i, '%c'}\n", __PRETTY_FUNCTION__, sp->ival, sp->cval);
 }
 
 void OutStructPtrParam(SomeStruct* osp)
 {
     osp->cval = 'o';
     osp->ival = 10;
-    printf("<= %30s :: {i=%i, c=%c}\n", __PRETTY_FUNCTION__, osp->ival, osp->cval);
+    printf("<= %30s :: {%i, '%c'}\n", __PRETTY_FUNCTION__, osp->ival, osp->cval);
 }
 
 int ReturnSomePodValue(int i)
--- swift/swift-cpp-interop-test/objctestapp/main.mm
+++ swift/swift-cpp-interop-test/objctestapp/main.mm
@@ -1,9 +1,24 @@
 #import <Foundation/Foundation.h>
+#include "lib.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
         NSLog(@"Hello, World!");
+        {
+            CppTestLib::TakeSomeCppParams("test123");
+
+            int i = 5;
+            CppTestLib::TakeAPtrParam(&i);
+
+            CppTestLib::SomeStruct inVal{30, 'Z'};
+            CppTestLib::TakeAStructPtrParam(&inVal);
+
+            CppTestLib::SomeStruct outVal;
+            CppTestLib::OutStructPtrParam(&outVal);
+
+            const int rv = CppTestLib::ReturnSomePodValue(10);
+            NSLog(@".. received %i from CppTestLib::ReturnSomePodValue", rv);
+        }
     }
     return 0;
 }
